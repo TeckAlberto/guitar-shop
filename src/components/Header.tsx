6432be175670e0f6d575d5f55e0eb023c45db723
Alberto gutierrez <jes.alberto.ing@gmail.com>
2024-07-25 11:21:58 -0600

    Initial commit: Add complete guitar shop app
    This commit includes the full implementation of a guitar shop application. The features of this app include:
    
    - Adding cart for the products
    - Calculating and displaying product in the cart
    - Adding o discount quantity of products of cart
    - Responsive design for mobile and desktop

---
+++ src/components/Header.tsx
@@ -0,0 +1,136 @@
+import { Dispatch, useMemo } from "react";
+import { CartItem } from "../types";
+import { CartActions } from "../recucers/cart-reducer";
+
+type HeaderProps = {
+  cart: CartItem[];
+  dispatch: Dispatch<CartActions>;
+};
+
+export default function Header({ cart, dispatch }: HeaderProps) {
+
+    const isEmpty = useMemo(() => cart.length === 0, [cart]);
+    const cartTotal = useMemo(
+      () =>
+        cart.reduce(
+          (total, { quantity, price }) => total + quantity * price,
+          0
+        ),
+      [cart]
+    );
+
+
+    return (
+      <header className="py-5 header">
+        <div className="container-xl">
+          <div className="row justify-content-center justify-content-md-between">
+            <div className="col-8 col-md-3">
+              <a href="index.html">
+                <img
+                  className="img-fluid"
+                  src="./img/logo.svg"
+                  alt="imagen logo"
+                />
+              </a>
+            </div>
+            <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+              <div className="carrito">
+                <img
+                  className="img-fluid"
+                  src="./img/carrito.png"
+                  alt="imagen carrito"
+                />
+
+                <div id="carrito" className="bg-white p-3">
+                  {isEmpty ? (
+                    <p className="text-center">El carrito esta vacio</p>
+                  ) : (
+                    <>
+                      <table className="w-100 table">
+                        <thead>
+                          <tr>
+                            <th>Imagen</th>
+                            <th>Nombre</th>
+                            <th>Precio</th>
+                            <th>Cantidad</th>
+                            <th></th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {cart.map(({ id, name, price, image, quantity }) => (
+                            <tr key={id}>
+                              <td>
+                                <img
+                                  className="img-fluid"
+                                  src={`./img/${image}.jpg`}
+                                  alt="imagen guitarra"
+                                />
+                              </td>
+                              <td>{name}</td>
+                              <td className="fw-bold">${price}</td>
+                              <td className="flex align-items-start gap-4">
+                                <button
+                                  type="button"
+                                  className="btn btn-dark"
+                                  onClick={() =>
+                                    dispatch({
+                                      type: "decrease-quantity",
+                                      payload: { id },
+                                    })
+                                  }
+                                >
+                                  -
+                                </button>
+                                {quantity}
+                                <button
+                                  type="button"
+                                  className="btn btn-dark"
+                                  onClick={() =>
+                                    dispatch({
+                                      type: "increase-quantity",
+                                      payload: { id },
+                                    })
+                                  }
+                                >
+                                  +
+                                </button>
+                              </td>
+                              <td>
+                                <button
+                                  className="btn btn-danger"
+                                  type="button"
+                                  onClick={() =>
+                                    dispatch({
+                                      type: "remove-from-cart",
+                                      payload: { id },
+                                    })
+                                  }
+                                >
+                                  X
+                                </button>
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+
+                      <p className="text-end">
+                        Total pagar:{" "}
+                        <span className="fw-bold">${cartTotal}</span>
+                      </p>
+                      <button
+                        className="btn btn-dark w-100 mt-3 p-2"
+                        onClick={ () => dispatch({type: 'clear-cart'})}
+                      >
+                        Vaciar Carrito
+                      </button>
+                    </>
+                  )}
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </header>
+    );
+}
